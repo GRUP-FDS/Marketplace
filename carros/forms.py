101d63eb3386799b96037adc1ed1884d02fd3e9b
arthurreis33 <arthur.ars33@gmail.com>
2023-04-10 11:11:46 -0300

    formulário de criação de anúncio

---
+++ carros/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from .models import Car
+
+INPUT_CLASSES = 'w-full mt-2 px-4 py-2 rounded-xl border border-2 border-gray-400'
+
+class NewCarForm(forms.ModelForm):
+  class Meta:
+    model = Car
+    fields = ('brand', 'car_model', 'year', 'type', 'price', 'color', 
+              'image', 'description')
+    labels = {
+      'brand': 'Marca',
+      'car_model':'Modelo',
+      'year':'Ano',
+      'type': 'Estado do Veículo',
+      'price': 'Preço',
+      'color':'Cor',
+      'description': 'Descrição',
+      'image': 'Imagem',
+
+    }
+
+    widgets = {
+      'brand': forms.TextInput(attrs={
+        'class': INPUT_CLASSES
+      }),
+      'car_model': forms.TextInput(attrs={
+        'class': INPUT_CLASSES
+      }),
+      'year': forms.NumberInput(attrs={
+        'class': INPUT_CLASSES
+      }),
+      'type': forms.TextInput(attrs={
+        'class': INPUT_CLASSES
+      }),
+      'price': forms.NumberInput(attrs={
+        'class': INPUT_CLASSES
+      }),
+      'color': forms.TextInput(attrs={
+        'class': INPUT_CLASSES
+      }),
+      'description': forms.TextInput(attrs={
+        'class': INPUT_CLASSES
+      }),
+      'image': forms.FileInput(attrs={
+        'class': INPUT_CLASSES
+      }),
+    }
--- carros/views.py
+++ carros/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render, redirect
+from .forms import NewCarForm
 
 # Create your views here.
 def new(request):
+  if request.method == 'POST':
+    form = NewCarForm(request.POST, request.FILES)
+
+    if form.is_valid():
+      item = form.save(commit=False)
+      item.save()
+
+      return redirect('home')
+  else:
+    form = NewCarForm()
+
   return render(request, 'carros/form.html', {
-    'title': 'Novo anúncio'
+    'form': form,
+    'title': 'Novo Anúncio'
   })
