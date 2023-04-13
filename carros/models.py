190c6215779ffe059f638db74bc9e6fce5e0f88a
Edmar Rocha Alencar <edmaaralencar1@gmail.com>
2023-04-13 10:44:41 -0300

    Ajeitar erro de imagem sem carro

--- carros/models.py
+++ carros/models.py
@@ -8,7 +8,7 @@ class Car(models.Model):
   type = models.CharField(max_length=255)
   price = models.FloatField()
   color = models.CharField(max_length=255)
-  image = models.ImageField(upload_to='car_images', blank=True, null=True)
+  image = models.ImageField(upload_to='car_images', blank=True, null=True, default='imagem-padrao.jpeg')
   description = models.TextField(blank=True, null=True)
   is_sold = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
