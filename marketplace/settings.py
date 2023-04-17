afffd2175210108d9d14a2ba59ee08f995fdf965
camilacirne <camilacirne@hotmail.com>
2023-04-17 12:06:06 -0300

    com quilometragem e tipo de combustivel na pdp

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wfc@w8cj_#)w%y%k0z00s7tl!nr!wg6d#0o%$wfkr^1t6+591(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cartradeo.us-east-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
