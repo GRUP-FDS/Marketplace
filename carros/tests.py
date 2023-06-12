697dcdfc2492f7e6e92b3c813a4a00e0c3088ba8
brunacarvalho202 <brubslubs2019@gmail.com>
2023-06-12 11:00:08 -0300

    vai

--- carros/tests.py
+++ carros/tests.py
@@ -4,17 +4,7 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-
-
-# #def setUp():
-#       #  chrome_options = webdriver.ChromeOptions()
-#        # chrome_options.add_argument('--icognito') 
-#         #chrome_options.add_argument('--headless') 
-#         #chrome_options.add_argument("--no-sandbox")
-#         #chrome_options.add_argument("--disable-gpu")
-#         driver = webdriver.Chrome(options=chrome_options)
-#         driver.maximize_window()
-#         return driver   
+from urllib3.util import Timeout
 
 def setUp():
     chrome_options = webdriver.ChromeOptions()
