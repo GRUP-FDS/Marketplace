1d754e8c8abec111476339701b518404a5c29296
Edmar Rocha Alencar <edmaaralencar1@gmail.com>
2023-06-11 21:10:41 -0300

    Ajeitar chat

--- chat/views.py
+++ chat/views.py
@@ -1,36 +1,46 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect
 
 from carros.models import Car
 from .models import Chat, Message
 
 # Create your views here.
 def chat(request, id):
-  buyerUsername = request.GET.get('buyer')
-  sellerUsername = request.GET.get('seller')
+  if request.method == 'POST':
+    message = request.POST.get('content')
+    buyerUsername = request.POST.get('user')
+    chatId = request.POST.get('chat')
 
-  car = Car.objects.get(pk=id)
-  buyer = User.objects.get(username=buyerUsername)
-  user = request.user
-  creator_of_car = car.created_by
+    chat = Chat.objects.get(id=int(chatId))
+    userBuyer = User.objects.get(username=buyerUsername)
 
-  messages = None
-  chat = None
+    Message.objects.create(user=userBuyer, content=message, chat=chat)
 
+    return redirect(request.META['HTTP_REFERER'])
 
-  try:
-    chat = Chat.objects.get(car=car, seller=creator_of_car, buyer=buyer)
+  else:
+    buyerUsername = request.GET.get('buyer')
 
-    messages = Message.objects.filter(chat=chat)
-  except ObjectDoesNotExist:
-    chat = Chat(buyer=user, seller=car.created_by, car=car)
-    chat.save()
+    car = Car.objects.get(pk=id)
+    buyer = User.objects.get(username=buyerUsername)
+    user = request.user
+    creator_of_car = car.created_by
 
-  print(messages)
+    messages = None
+    chat = None
+
+    try:
+      chat = Chat.objects.get(car=car, seller=creator_of_car, buyer=buyer)
+
+      messages = Message.objects.filter(chat=chat)
+    except ObjectDoesNotExist:
+      chat = Chat(buyer=user, seller=car.created_by, car=car)
+      chat.save()
+
   return render(request, 'chat/index.html', {
     'car': car,
     'chat': chat,
     'messages': messages
   })
-
--- marketplace/asgi.py
+++ marketplace/asgi.py
@@ -1,19 +1,26 @@
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+
+# import chat.routing
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
+
+# application = ProtocolTypeRouter({
+#   'http': get_asgi_application(),
+#   'websocket': AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         )
+#     )
+# })
 import os
 
 from django.core.asgi import get_asgi_application
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-import chat.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
-
-application = ProtocolTypeRouter({
-  'http': get_asgi_application(),
-  'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    )
-})
+application = get_asgi_application()
