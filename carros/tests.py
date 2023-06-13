a01984aa3767b723d73b7574d98f2d5b59f244fb
Guilherme Lopes <75703772+Juillerms@users.noreply.github.com>
2023-06-12 23:34:58 -0300

    Testes novos atualizados

--- carros/tests.py
+++ carros/tests.py
@@ -219,85 +219,86 @@ class MySeleniumTest(LiveServerTestCase):
     #     assert mensagem_sem_anuncio.text == "Você ainda não tem nenhum anúncio. Crie um agora."
 
     #     #------------------------------------------------------------------------------------------------------------#
-    # def test_anuncio_vendido(self):
+     def test_anuncio_vendido(self):
 
-    #     browser = setUp()
-    #     browser.get('http://127.0.0.1:8000/')
-    #     sleep(3)
-    #     browser.find_element(By.ID,'logar').click()
-    #     input_user = browser.find_element(By.ID,'user')
-    #     input_senha = browser.find_element(By.ID,'senha')
-    #     input_user.send_keys('lulu')
-    #     input_senha.send_keys('px132652')
-    #     browser.find_element(By.ID,'login-registrar').click()
-    #     sleep(3)
+         browser = setUp()
+         browser.get('http://127.0.0.1:8000/')
+         sleep(3)
+         browser.find_element(By.ID,'logar').click()
+         input_user = browser.find_element(By.ID,'user')
+         input_senha = browser.find_element(By.ID,'senha')
+         input_user.send_keys('antoniosilva')
+         input_senha.send_keys('mochimochi')
+         browser.find_element(By.ID,'login-registrar').click()
+         sleep(3)
 
         
-    #     browser.find_element(By.ID,'criar-anuncios').click()
-    #     sleep(3)
-    #     input_marca = browser.find_element(By.ID,'id_brand')
-    #     input_model = browser.find_element(By.ID,'id_car_model')
-    #     input_km = browser.find_element(By.ID,'id_mileage')
-    #     input_ano = browser.find_element(By.ID,'id_year')
-    #     input_combustivel = browser.find_element(By.ID,'id_fuel_type')
-    #     input_estado = browser.find_element(By.ID,'id_type')
-    #     input_price = browser.find_element(By.ID,'id_price')
-    #     input_color = browser.find_element(By.ID,'id_color')
-    #     input_image = browser.find_element(By.ID, 'id_image')   
-    #     input_descricao = browser.find_element(By.ID,'id_description')
-    #     sleep(2) 
+         browser.find_element(By.ID,'criar-anuncios').click()
+         sleep(3)
+         input_marca = browser.find_element(By.ID,'id_brand')
+         input_model = browser.find_element(By.ID,'id_car_model')
+         input_km = browser.find_element(By.ID,'id_mileage')
+         input_ano = browser.find_element(By.ID,'id_year')
+         input_combustivel = browser.find_element(By.ID,'id_fuel_type')
+         input_estado = browser.find_element(By.ID,'id_type')
+         input_price = browser.find_element(By.ID,'id_price')
+         input_color = browser.find_element(By.ID,'id_color')
+         input_image = browser.find_element(By.ID, 'id_image')   
+         input_descricao = browser.find_element(By.ID,'id_description')
+         sleep(2) 
+      
+         #preenchendo campos
+         input_marca.send_keys('Produto de Teste')
+         input_model.send_keys('Este é um produto de teste')
+         input_km.send_keys('10')
+         input_ano.send_keys('2023')
+         input_combustivel.send_keys('2023')
+         input_estado.send_keys('2023')
+         input_price.send_keys('2023')
+         input_color.send_keys('2023')
+         #input_image.send_keys('image.padrao')
+         input_descricao.send_keys('jsjshjshsjhjk')
+         browser.find_element(By.ID,'botao-criar-produto').click()    
+         sleep(3)
        
-    #     #preenchendo campos
-    #     input_marca.send_keys('Produto de Teste')
-    #     input_model.send_keys('Este é um produto de teste')
-    #     input_km.send_keys('10')
-    #     input_ano.send_keys('2023')
-    #     input_combustivel.send_keys('2023')
-    #     input_estado.send_keys('2023')
-    #     input_price.send_keys('2023')
-    #     input_color.send_keys('2023')
-    #     #input_image.send_keys('image.padrao')
-    #     input_descricao.send_keys('jsjshjshsjhjk')
-    #     browser.find_element(By.ID,'botao-criar-produto').click()    
-    #     sleep(3)
-        
-    #     #ENTRANDO NOS MEUS ANUNCIOS
-    #     browser.find_element(By.ID,'botao-meus-anuncios').click()
-    #     sleep(3)
-    #     browser.find_element(By.ID,'descricao_my_ads').click()
-    #     browser.find_element(By.ID,'marcar_vendido').click()
-    #     sleep(2)
+         #ENTRANDO NOS MEUS ANUNCIOS
+         browser.find_element(By.ID,'botao-meus-anuncios').click()
+         sleep(3)
+         browser.find_element(By.ID,'descricao_my_ads').click()
+         browser.find_element(By.ID,'marcar_vendido').click()
+         sleep(2)
 
-    #     mensagem_anuncio_vendido = browser.find_element(By.ID,'vendido')
-    #     assert mensagem_anuncio_vendido.text == 'Vendido'
+         mensagem_anuncio_vendido = browser.find_element(By.ID,'vendido')
+         assert mensagem_anuncio_vendido.text == 'Vendido'
 
 
     #     #------------------------------------------------------------------------------------------------------------#
-    # def test_atualizar_produto(self): 
-    #     browser = setUp()
-    #     browser.get('http://127.0.0.1:8000/')
-    #     browser.find_element(By.ID,'logar').click()
-    #     input_user = browser.find_element(By.ID,'user')
-    #     input_senha = browser.find_element(By.ID,'senha')
-    #     input_user.send_keys('brunaamamuito')
-    #     input_senha.send_keys('thiagopreto')
-    #     browser.find_element(By.ID,'login-registrar').click()
-    #     sleep(3)
+     def test_atualizar_produto(self): 
+         browser = setUp()
+         browser.get('http://127.0.0.1:8000/')
+         browser.find_element(By.ID,'logar').click()
+         input_user = browser.find_element(By.ID,'user')
+         input_senha = browser.find_element(By.ID,'senha')
+         input_user.send_keys('brunaamamuito')
+         input_senha.send_keys('thiagopreto')
+         browser.find_element(By.ID,'login-registrar').click()
+         sleep(3)
         
-    #     browser.find_element(By.ID,'botao-meus-anuncios').click()
-    #     browser.find_element(By.ID,'atualizar_my_ads').click() #id do botao "atualizar"
+         browser.find_element(By.ID,'botao-meus-anuncios').click()
+         browser.find_element(By.ID,'atualizar_my_ads').click() #id do botao "atualizar"
 
-    #     #atualizando os campos do anuncio
-    #     input_descricao = browser.find_element(By.ID,'id_description')
-    #     input_descricao.send_keys('Atualização do campo de descrição')
-    #     browser.find_element(By.ID,'botao-criar-produto').click() #id do botao de "atualizar produto"
-    #     sleep(3)
+         #atualizando os campos do anuncio
+         input_descricao = browser.find_element(By.ID,'id_description')
+         input_descricao.clear()
+         input_descricao.send_keys('Atualização do campo de descrição')
+         browser.find_element(By.ID,'botao-criar-produto').click() #id do botao de "atualizar produto"
+         sleep(3)
 
-    #     descricao_descricao = browser.find_element(By.ID,'descricao_descricao')
-    #     assert descricao_descricao.text == 'Atualização do campo de descrição'
+         descricao_descricao = browser.find_element(By.ID,'descricao_descricao')
+         assert descricao_descricao.text == 'Atualização do campo de descrição'
         #------------------------------------------------------------------------------------------------------------#
 
-    def test_minhas_conversas(self):
+     def test_minhas_conversas(self):
         browser = setUp()
         browser.get('http://127.0.0.1:8000/')
         browser.find_element(By.ID,'logar').click()
@@ -309,16 +310,17 @@ class MySeleniumTest(LiveServerTestCase):
         sleep(3)
 
         browser.find_element(By.ID,'minhas-conversas').click()
-        browser.find_element(By.ID,'oi').click()
         sleep(2)
+        # browser.find_element(By.ID,'oi').click()
+        # sleep(2)
 
-        input_mensagem = browser.find_element(By.ID,'chat-message-input')
-        sleep(1)
-        input_mensagem.send_keys('oioioi')
-        sleep(2)
+        # input_mensagem = browser.find_element(By.ID,'chat-message-input')
+        # sleep(1)
+        # input_mensagem.send_keys('oioioi')
+        # sleep(2)
 
-        mensagem_exemplo = browser.find_element(By.ID,'mensagem_exemplo')
-        assert mensagem_exemplo.text == 'oioioi'
+        mensagem_exemplo = browser.find_element(By.ID,'tela_conversas')
+        assert mensagem_exemplo.text == 'Minhas conversas'
 
 
         
