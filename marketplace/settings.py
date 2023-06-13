1ac15bb7660713db8d9fd9e50dfd0366863e09c5
Bruna Bezerra <brubslubs2019@gmail.com>
2023-06-12 21:12:11 -0300

    criando banco de dados para os testes

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -91,6 +91,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'utils.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
     }
 }
 
